2551461a6fb8639a153ff5cfdd62532c02f23cb2
orsk-moscow <moscow.orsk@gmail.com>
2020-10-15 11:08:33 +0300

    курс степик: начал решать проверочный тест чемп яндекс: начал решать трек по алгосам

---
+++ input.py
@@ -0,0 +1,14 @@
+# single line with integers
+arr = list(map(int, (str(input()).split())))
+## 2 params
+n = arr[0]
+k = arr[1]
+
+# single line with strings
+arr = list(str(input()).split())
+n = int(arr[0])
+m = int(arr[1])
+
+# pure string
+arr = input()
+
